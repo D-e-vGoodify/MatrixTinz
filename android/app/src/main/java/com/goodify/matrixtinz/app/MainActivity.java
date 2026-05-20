0c2199344a055277ae2354ed7b7c8602bf658813
D-e-vGoodify <agoodness922@gmail.com>
2026-05-20 01:04:12 +0100

    update MainActivity.java

--- android/app/src/main/java/com/goodify/matrixtinz/app/MainActivity.java
+++ android/app/src/main/java/com/goodify/matrixtinz/app/MainActivity.java
@@ -6,16 +6,17 @@ import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
   @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Access the underlying WebView and its settings
-        WebSettings settings = this.getBridge().getWebView().getSettings();
-        
-        // Force the WebView to point its font aliases to the system defaults
-        settings.setStandardFontFamily("sans-serif");
-        settings.setSansSerifFontFamily("sans-serif");
-        settings.setSerifFontFamily("serif");
-        settings.setMonospaceFontFamily("monospace");
-    }
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    
+    // 2. Access the underlying WebView safely
+    WebView webView = (WebView) this.bridge.getWebView();
+    WebSettings settings = webView.getSettings();
+    
+    // 3. Force the WebView to point its font aliases to the system defaults
+    settings.setStandardFontFamily("sans-serif");
+    settings.setSansSerifFontFamily("sans-serif");
+    settings.setSerifFontFamily("serif");
+    settings.setMonospaceFontFamily("monospace");
+  }
 }
